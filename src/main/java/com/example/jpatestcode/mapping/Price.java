73b0d951f1966f98efe6725033f3ab7d57439a34
mk1-p <mk1.dev662@gmail.com>
2023-12-07 17:26:46 +0900

    ENH: Add createdDt

--- src/main/java/com/example/jpatestcode/mapping/Price.java
+++ src/main/java/com/example/jpatestcode/mapping/Price.java
@@ -7,8 +7,15 @@ import com.example.jpatestcode.mapping.superclass.SuperProduct;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
+@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Price {
 
@@ -29,4 +36,15 @@ public class Price {
     @Column(name = "super_id")
     private Long superId;
 
+    @CreatedBy
+    private LocalDateTime createdDt;
+
+    @Builder
+    public Price(Long id, Integer cost, JoinedProduct joinedProduct, SingleProduct singleProduct, Long superId) {
+        this.id = id;
+        this.cost = cost;
+        this.joinedProduct = joinedProduct;
+        this.singleProduct = singleProduct;
+        this.superId = superId;
+    }
 }
